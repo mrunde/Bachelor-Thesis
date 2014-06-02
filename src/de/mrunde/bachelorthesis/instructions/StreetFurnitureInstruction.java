68dcc43512309cf85ccfab72dd2c9126978eff1a
Marius Runde <marius.runde@uni-muenster.de>
2014-06-02 09:30:19 +0200

    Initial implementation of the StreetFurnitureInstruction

--- src/de/mrunde/bachelorthesis/instructions/StreetFurnitureInstruction.java
+++ src/de/mrunde/bachelorthesis/instructions/StreetFurnitureInstruction.java
@@ -2,6 +2,10 @@ package de.mrunde.bachelorthesis.instructions;
 
 import com.mapquest.android.maps.GeoPoint;
 
+import de.mrunde.bachelorthesis.basics.Maneuver;
+import de.mrunde.bachelorthesis.basics.StreetFurniture;
+import de.mrunde.bachelorthesis.basics.StreetFurnitureCategory;
+
 /**
  * This is a "street furniture"-based instruction.
  * 
@@ -10,9 +14,36 @@ import com.mapquest.android.maps.GeoPoint;
 public class StreetFurnitureInstruction extends Instruction {
 
 	/**
-	 * Constructor of the StreetFurnitureInstruction class
+	 * Street furniture at the decision point
 	 */
-	public StreetFurnitureInstruction(GeoPoint decisionPoint, int maneuverType) {
+	private StreetFurniture streetFurniture;
+
+	/**
+	 * Constructor of the StreetFurnitureInstruction class
+	 * 
+	 * @param decisionPoint
+	 *            The decision point
+	 * @param maneuverType
+	 *            The maneuver type
+	 * @param streetFurniture
+	 *            The street furniture at the decision point
+	 */
+	public StreetFurnitureInstruction(GeoPoint decisionPoint, int maneuverType,
+			StreetFurniture streetFurniture) {
 		super(decisionPoint, maneuverType);
+		this.streetFurniture = streetFurniture;
+	}
+
+	/**
+	 * @return The Instruction as a verbal text
+	 */
+	public String toString() {
+		if (Maneuver.isTurnAction(super.getManeuverType())) {
+			String instruction = super.getManeuver() + " after the "
+					+ this.streetFurniture.toString();
+			return instruction;
+		} else {
+			return null;
+		}
 	}
 }
