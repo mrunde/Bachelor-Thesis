9f25566fd18770b106aa703094fa632c5e9a61de
Marius Runde <marius.runde@uni-muenster.de>
2014-06-03 16:07:34 +0200

    StreetFurnitureInstruction can now handle two street furniture

--- src/de/mrunde/bachelorthesis/instructions/StreetFurnitureInstruction.java
+++ src/de/mrunde/bachelorthesis/instructions/StreetFurnitureInstruction.java
@@ -18,7 +18,13 @@ public class StreetFurnitureInstruction extends Instruction {
 	private StreetFurniture streetFurniture;
 
 	/**
-	 * Constructor of the StreetFurnitureInstruction class
+	 * Second street furniture before the decision point (optional)
+	 */
+	private StreetFurniture secondStreetFurniture;
+
+	/**
+	 * Constructor of the StreetFurnitureInstruction class for an instruction
+	 * from one street furniture
 	 * 
 	 * @param decisionPoint
 	 *            The decision point
@@ -34,12 +40,48 @@ public class StreetFurnitureInstruction extends Instruction {
 	}
 
 	/**
+	 * Constructor of the StreetFurnitureInstruction class for an instruction
+	 * from two street furniture
+	 * 
+	 * @param decisionPoint
+	 *            The decision point
+	 * @param maneuverType
+	 *            The maneuver type
+	 * @param streetFurniture
+	 *            The street furniture at the decision point
+	 * @param secondStreetFurniture
+	 *            The street furniture before the decision point
+	 */
+	public StreetFurnitureInstruction(GeoPoint decisionPoint, int maneuverType,
+			StreetFurniture streetFurniture,
+			StreetFurniture secondStreetFurniture) {
+		super(decisionPoint, maneuverType);
+		this.streetFurniture = streetFurniture;
+		// Check if the street furniture have the same category
+		if (this.streetFurniture.getCategory().equals(
+				secondStreetFurniture.getCategory())) {
+			this.secondStreetFurniture = secondStreetFurniture;
+		} else {
+			this.secondStreetFurniture = null;
+		}
+	}
+
+	/**
 	 * @return The instruction as a verbal text
 	 */
 	public String toString() {
 		if (Maneuver.isTurnAction(super.getManeuverType())) {
-			String instruction = super.getManeuver() + " after the "
-					+ this.streetFurniture.getCategory();
+			String instruction;
+			// Check if the second street furniture is set
+			if (this.secondStreetFurniture != null) {
+				// Create an instruction with two street furniture
+				instruction = super.getManeuver() + " after the second "
+						+ this.streetFurniture.getCategory();
+			} else {
+				// Create an instruction with one street furniture
+				instruction = super.getManeuver() + " after the "
+						+ this.streetFurniture.getCategory();
+			}
 			return instruction;
 		} else {
 			return null;
