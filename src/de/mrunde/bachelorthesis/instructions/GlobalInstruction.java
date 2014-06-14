2e3a48ab267268cd56c4029cd9a2b2abeded66f3
Marius Runde <marius.runde@uni-muenster.de>
2014-06-14 18:20:42 +0200

    Initial commit of GlobalInstruction

---
+++ src/de/mrunde/bachelorthesis/instructions/GlobalInstruction.java
@@ -0,0 +1,54 @@
+package de.mrunde.bachelorthesis.instructions;
+
+import com.mapquest.android.maps.GeoPoint;
+
+import de.mrunde.bachelorthesis.basics.Landmark;
+
+/**
+ * This is a "global landmark"-based instruction.
+ * 
+ * @author Marius Runde
+ */
+public class GlobalInstruction extends Instruction {
+
+	/**
+	 * The global landmark along the route
+	 */
+	private Landmark global;
+
+	/**
+	 * Constructor of the GlobalInstruction class
+	 * 
+	 * @param instructionPoint
+	 *            The point where the instruction should be spoken
+	 * @param global
+	 *            The global landmark along the route
+	 */
+	public GlobalInstruction(GeoPoint instructionPoint, Landmark global) {
+		super(instructionPoint, 0);
+		this.global = global;
+	}
+
+	/**
+	 * @return The instruction as a verbal text
+	 */
+	public String toString() {
+		String instruction = "You will pass ";
+		String category = this.global.getFormattedCategory();
+		if (category.startsWith("a") || category.startsWith("e")
+				|| category.startsWith("i") || category.startsWith("o")
+				|| category.startsWith("u")) {
+			instruction = "an " + category + " soon";
+		} else {
+			instruction = "a " + category + " soon";
+		}
+		return instruction;
+	}
+
+	/**
+	 * @return The global landmark along the route
+	 */
+	public Landmark getGlobal() {
+		return this.global;
+	}
+}
